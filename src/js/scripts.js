5e172ed187019152db774f69da538024fbe05e29
Keith Wallace <jkwallac@ualberta.ca>
2023-01-15 14:43:27 -0700

    comments for assignment outline added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,43 @@
+// write class for car
+
+
+// write class for engine
+// both have their own constructor
+
+// cars odometer initalize to 0
+// engine running initalize to false
+
+//other properties passed in through constructor parameters
+
+//constructor of car instantiate an engine aswell
+
+//properties for car Make, Model, Year, Odometer and engine
+
+//engine will be instance of engine class
+
+//methods of >>>>>car class
+//start car engine(no parameters) set engine to run
+//stop car engine(no parameters) set engine to not running
+//drive the car(single parameter representing the distance to drive) which will add the argument to the odometer if the engine is on and throw and exception otherwise
+
+//properties of engine class should include number of cylinders, wether its running or not
+
+//once class is constructed, write script in console that will (remember to catch exceptions)
+//1 Instatniate a car
+//2 Turn its engine on
+//3 Drive for 100km
+//4 Turn the engine off
+//5 Turn its engine on
+//6 Drive for 50km
+//7 Turn the engine off
+//8 Output the odometer reading to the console
+
+//No user input required
+
+//Finally output a JSON string representing your car object to the console
+
+
+
 async function main() {
    
 }
