85d7b5afd9497f1f1234dd9b998180eef6e5486f
Keith Wallace <jkwallac@ualberta.ca>
2023-01-15 18:13:35 -0700

    Start Engine method on Car class working

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,15 @@
-// write class for car
+// write class for car and engine
+// both have their own constructor
 //properties for car Make, Model, Year, Odometer and engine
 class Car {
-    constructor(carMake = "DMC", carModel = "Delorean", carYear = "1982", carEngine = "BLANK") {
+    constructor(carMake = "DMC", carModel = "Delorean", carYear = "1982", carEngine = new Engine()) {
         this.carMake = carMake;
         this.carModel = carModel;
         this.carYear = carYear;
         // cars odometer initalize to 0
         this.carOdometer = 0;
         this.carEngine = carEngine;
-        
+
     }
     //Declare outside constructor for language agnosticism (Not required for Javascript if they have a value in the constructor)
     carMake;
@@ -29,40 +30,73 @@ class Car {
         this.#carOdometer = incomingValue;
 
     }
-    
+
     //drive the car(single parameter representing the distance to drive) which will add the argument to the odometer if the engine is on and throw and exception otherwise
     drive(kmTravelled) {
         let kmToAdd = kmTravelled;
-        
+
         let newOdometerReading = this.carOdometer + kmToAdd;
         this.carOdometer = newOdometerReading;
 
     }
+    //methods of >>>>>car class
+    //start car engine(no parameters) set engine to run
+    startEngine() {
+        
 
-   
+        this.carEngine.engineRunning = true;
+
+
+    }
+    //stop car engine(no parameters) set engine to not running
+    stopEngine() {
+
+    }
+
+
 }
 
 
 // write class for engine
-// both have their own constructor
+//properties of engine class should include number of cylinders, wether its running or not
+
+class Engine {
+    constructor(engineRunning = false, engineCylinders = 6,) {
+        // engine running initalize to false
+        this.engineRunning = engineRunning;
+        this.engineCylinders = engineCylinders;
+    }
+    //Declare outside constructor for language agnosticism (Not required for Javascript if they have a value in the constructor)
+    engineRunning;
+    engineCylinders;
+    
+
+    get engineRunning() {
+        return this.engineRunning;
+    }
+    set engineRunning(incomingValue) {
+        if (this.engineRunning == true) {
+            throw new Error("The car is already running");
+        }
+        this.engineRunning = incomingValue;
+
+    }
+    toString() {
+        return `A ${this.engineCylinders} Cylinder Engine that is ${this.engineRunning}`
+    }
+}
 
 
-// engine running initalize to false
+
+
 
 //other properties passed in through constructor parameters
 
 //constructor of car instantiate an engine aswell
 
 
-
 //engine will be instance of engine class
 
-//methods of >>>>>car class
-//start car engine(no parameters) set engine to run
-//stop car engine(no parameters) set engine to not running
-
-
-//properties of engine class should include number of cylinders, wether its running or not
 
 //once class is constructed, write script in console that will (remember to catch exceptions)
 //1 Instatniate a car
@@ -87,11 +121,18 @@ async function main() {
     try {
         myCar.drive(-20);
     }
-    catch (error)
-    {
+    catch (error) {
         output(error);
     }
     output(myCar.carOdometer);
+    
+    output(myCar.carEngine);
+    output(myCar.carEngine.engineRunning);
+
+    myCar.startEngine();
+    output(myCar.carEngine.engineRunning);
+    
+
 
 
 }
